10ab6d68d31854fd8564163f82c3a3a47853a7bf
abhishekk1004 <abhiobray143@gmail.com>
2025-06-22 22:17:20 +0545

    models.py

--- checking/models.py
+++ checking/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class UploadedImage(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     image = models.ImageField(upload_to='uploads/')
